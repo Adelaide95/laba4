b73d50d4d239d783b612188bedca375fddb455b4
Adelaide95 <Adelaide2002@mail.ru>
2021-04-21 00:59:58 +0300

    add new

---
+++ individ1.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+if __name__=='__main__':
+    a = int(input("Введите  число"))
+
+    if abs(a) == 1:
+        print('one')
+        exit()
+    elif abs(a) == 2:
+        print('two')
+        exit()
+    elif abs(a) == 3:
+        print('three')
+        exit()
+    elif abs(a) == 4:
+        print('four')
+        exit()
+    elif abs(a) == 5:
+        print('five')
+        exit()
+    elif abs(a) == 6:
+        print('six')
+        exit()
+    elif abs(a) == 7:
+        print('seven')
+        exit()
+    elif abs(a) == 8:
+        print('eight')
